460adf61c6b59186c4a1ed6b08458a00efde1419
Hye-Yon Song <hye-yonsong@Hye-Yons-MacBook-Pro.local>
2022-11-13 16:00:12 -0800

    updates

--- scripts.js
+++ scripts.js
@@ -1,5 +1,15 @@
-//have user to be able to add a todo and add it
+var data = localStorage.getItem("listItem").split(",");
+console.log(data)
 
+for (let i = 0; i < data.length; i++) {
+    var todoText = data[i]
+    var listItem = document.createElement("li")
+    listItem.textContent = todoText 
+    listItem.classList.add("list-group-item")
+    document.querySelector(".list-group").append(listItem)
+  }
+
+//have user to be able to add a todo and add i
 document.querySelector(".btn.btn-outline-secondary").addEventListener("click", function (e) {
     //get value from input
     var todoText = document.querySelector(".form-control").value
@@ -9,6 +19,12 @@ document.querySelector(".btn.btn-outline-secondary").addEventListener("click", f
     console.log(listItem)
     listItem.classList.add("list-group-item")
     document.querySelector(".list-group").append(listItem)
+    //localStorage.getItem(listItem)
+    var initData = localStorage.getItem("listItem")
+    var newData = initData + ',' + todoText
+    console.log(newData)
+    
+    localStorage.setItem("listItem", newData)
     }
     );
 
