2d794b51b5b8b84e6aaa1eebdb98b845b63d40d1
Hye-Yon Song <hye-yonsong@Hye-Yons-MacBook-Pro.local>
2022-11-27 16:08:41 -0800

    updates

--- scripts.js
+++ scripts.js
@@ -1,47 +1,46 @@
-var list = localStorage.getItem("listItem")
+var list = localStorage.getItem("listItem");
 
 var data;
 
 if (list) {
-    data = list.split(",");
+  data = list.split(",");
 } else {
-    data = ""
+  data = "";
 }
 
-console.log(data)
+console.log(data);
 
 for (let i = 0; i < data.length; i++) {
-    var todoText = data[i]
-    var listItem = document.createElement("li")
-    listItem.textContent = todoText 
-    listItem.classList.add("list-group-item")
-    document.querySelector(".list-group").append(listItem)
-  }
+  var todoText = data[i];
+  var listItem = document.createElement("li");
+  listItem.textContent = todoText;
+  listItem.classList.add("list-group-item");
+  document.querySelector(".list-group").append(listItem);
+}
 
 //have user to be able to add a todo and add i
-document.querySelector(".btn.btn-outline-secondary").addEventListener("click", function (e) {
+document
+  .querySelector(".btn.btn-outline-secondary")
+  .addEventListener("click", function (e) {
     //get value from input
-    var todoText = document.querySelector(".form-control").value
-    //turn string item into list item
-    var listItem = document.createElement("li")
-    listItem.textContent = todoText
-    console.log(listItem)
-    listItem.classList.add("list-group-item")
-    document.querySelector(".list-group").append(listItem)
-    //localStorage.getItem(listItem)
-    var initData = localStorage.getItem("listItem") || ""
-    var newData = initData + ',' + todoText
-    console.log(newData)
-    
-    localStorage.setItem("listItem", newData)
+    var todoText = document.querySelector(".form-control").value;
+
+    if ((todoText === "")) {
+      return
+    } else {
+      //turn string item into list item
+      var listItem = document.createElement("li")
+      listItem.textContent = todoText
+      listItem.classList.add("list-group-item")
+      document.querySelector(".list-group").append(listItem)
+      localStorage.getItem(listItem)
+      var initData = localStorage.getItem("listItem") || ""
+      var newData = initData + ',' + todoText
+       localStorage.setItem("listItem", newData)
     }
-    );
+  });
 
 document.querySelector(".btn-delete").addEventListener("click", function (e) {
-        localStorage.clear()
-        window.location.reload()
-    } 
-    );
-
-
-
+  localStorage.clear();
+  window.location.reload();
+});
